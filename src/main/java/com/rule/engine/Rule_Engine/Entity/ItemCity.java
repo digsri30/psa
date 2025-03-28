252dc48bcacf69387984c038942ee4d6a31fdfb5
sri <you@example.com>
2025-03-28 15:21:09 +0530

    first

---
+++ src/main/java/com/rule/engine/Rule_Engine/Entity/ItemCity.java
@@ -0,0 +1,11 @@
+package com.rule.engine.Rule_Engine.Entity;
+
+public class ItemCity {
+    public enum city{
+        Pune,nagpur
+    }
+    public enum Type {
+        GROCERIES, MEDICINES, WATCHES, LUXUARYGOODS
+    }
+
+}
