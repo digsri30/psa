ba95134bdee08160a4b981500c269b7270164973
digsri30 <srishtijnv86@gmail.com>
2025-03-28 15:41:14 +0530

    Update ItemCity.java

--- src/main/java/com/rule/engine/Rule_Engine/Entity/ItemCity.java
+++ src/main/java/com/rule/engine/Rule_Engine/Entity/ItemCity.java
@@ -2,7 +2,7 @@ package com.rule.engine.Rule_Engine.Entity;
 
 public class ItemCity {
     public enum city{
-        Pune,nagpur
+        Pune,nagpur,delhi
     }
     public enum Type {
         GROCERIES, MEDICINES, WATCHES, LUXUARYGOODS
